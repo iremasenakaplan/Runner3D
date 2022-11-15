8b2a3e463cbb28b31acf53c12713d75821d54e8b
iremasenakaplan <iremasenakaplan19@gmail.com>
2022-11-15 21:23:54 +0300

    Contact with obstacles in the game

--- Assets/Script/CollectCoin.cs
+++ Assets/Script/CollectCoin.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; //textMesh e ulasmak icin
 
 public class CollectCoin : MonoBehaviour
 {
+  public int Score;
+  public TextMeshProUGUI CoinText;
     
    private void OnTriggerEnter(Collider other)
    {
        if(other.CompareTag("Coin"))
        {
+         AddCoin();
          Destroy(other.gameObject); // coin temas halinde kaybolur
        }
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+       if(collision.gameObject.CompareTag("Collision"))
+       {
+          Debug.Log("Touched obstacle...");
+       }
+   }
+
+   public void AddCoin()
+   {
+       Score++;
+       CoinText.text = "Score: " + Score.ToString();
    }
 }
